22a2ad2b28849c477a786d81ffafeeeb6520cba7
OwenHongJinmu <bjl1@163.com>
2017-02-23 14:51:28 +0800

    init

---
+++ AccountBook/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AccountBook
+//
+//  Created by OwenHong on 2017/2/23.
+//  Copyright © 2017年 owenhong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
